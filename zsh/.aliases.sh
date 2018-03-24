548e8c60c7ebca2e03718cbc3a9aeeece54df8bd
Tomas Van Roose <tomas.vanroose@gmail.com>
2018-03-24 23:38:29 +0100

    Add vim alias

--- zsh/.aliases.sh
+++ zsh/.aliases.sh
@@ -3,6 +3,8 @@ alias ls="ls -lG"
 alias config="$EDITOR $HOME/.zshrc"
 alias reload="source $HOME/.zshrc"
 
+alias vimrc="$EDITOR $HOME/.vimrc"
+
 alias ugent="cd ~/Documents/UGent/Master1/Semester2/"
 alias it="cd ~/Documents/UGent/WiNA/IT/"
 
