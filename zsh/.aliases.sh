c058d616645aa14e213fdbf24a1270eb41f4dab0
Tomas Van Roose <tomas.vanroose@gmail.com>
2018-03-24 21:11:28 +0100

    Add aliases

---
+++ zsh/.aliases.sh
@@ -0,0 +1,7 @@
+alias config="$EDITOR $HOME/.zshrc"
+alias reload="source $HOME/.zshrc"
+
+alias ugent="cd ~/Documents/UGent/Master1/Semester2/"
+alias it="cd ~/Documents/UGent/WiNA/IT/"
+
+alias compile="./compile.sh"
