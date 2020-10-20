a0ad1d843b4077d36afb4d4db86f645240a56a13
Tomas Van Roose <tomas.vanroose@gmail.com>
2020-10-20 23:59:26 +0200

    Remove old school aliases

--- zsh/.aliases.sh
+++ zsh/.aliases.sh
@@ -6,9 +6,7 @@ alias reload="source $HOME/.zshrc"
 alias vimrc="$EDITOR $HOME/.config/nvim/init.vim"
 alias vim="nvim"
 
-alias ugent="cd ~/Documents/UGent/Master2/Semester2/"
-alias cultour="cd ~/Documents/UGent/Cultour/"
-alias stage="cd ~/Documents/UGent/Stage/"
-alias it="cd ~/Documents/UGent/WiNA/IT/"
+alias ugent="cd ~/UGent/Master/"
+alias thesis="cd ~/UGent/Master/Thesis"
 
 alias compile="./compile.sh"
